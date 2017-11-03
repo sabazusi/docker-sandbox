922b7ba7da68afacd3a1be12c1e6228bae74d030
sabazusi <kouhei.live@gmail.com>
2017-11-03 22:56:48 +0900

    feat: add docker files

--- systems-1/server/main.go
+++ systems-1/server/main.go
@@ -11,5 +11,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   http.HandleFunc("/api", handler)
-  http.ListenAndServe(":8080", nil)
+  http.ListenAndServe(":3333", nil)
 }
